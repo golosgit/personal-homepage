340eda19986f23459425468bf3507f49b38dd9ca
golosgit <107067096+golosgit@users.noreply.github.com>
2022-10-18 18:36:18 +0200

    fixed app centering

--- src/components/AppContainer/index.js
+++ src/components/AppContainer/index.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 
 export const AppContainer = styled.div`
   max-width: 1216px;
-  margin: 0 16px;
+  margin: auto;
+
+  @media (max-width: 1248px) {
+    margin: 0 16px;
+  }
 `;
