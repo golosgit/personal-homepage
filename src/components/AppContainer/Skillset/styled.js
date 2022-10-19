cbfb60bf2b85b81b1d3fc11fbd794f7ce19134ed
golosgit <107067096+golosgit@users.noreply.github.com>
2022-10-19 18:54:55 +0200

    improved styles

--- src/components/AppContainer/Skillset/styled.js
+++ src/components/AppContainer/Skillset/styled.js
@@ -23,29 +23,21 @@ export const ListWrapper = styled.div`
 `;
 
 export const List = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
   color: ${({ theme }) => theme.color.slateGray};
   font-weight: 400;
   font-size: 18px;
   list-style: none;
   line-height: 25px;
   padding: 0;
-  column-count: 3;
   margin: 32px 0 0 0;
-
-  @media (max-width: 1100px) {
-    column-count: 2;
-  }
-
-  @media (max-width: 767px) {
-    column-count: 1;
-  }
 `;
 
 export const ListItem = styled.li`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
   margin-bottom: 8px;
+  flex-basis: 33.3%;
 
   &:before {
     content: "";
@@ -55,5 +47,13 @@ export const ListItem = styled.li`
     height: 9px;
     border-radius: 50%;
     background: ${({ theme }) => theme.color.scienceBlue};
+  }
+
+  @media (max-width: 1100px) {
+    flex-basis: 50%;
+  }
+
+  @media (max-width: 767px) {
+    flex-basis: 100%;
   }
 `;
