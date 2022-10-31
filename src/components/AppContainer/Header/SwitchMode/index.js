775c4b029b20382b38e0c694f93d78bcfdff9fcd
golosgit <107067096+golosgit@users.noreply.github.com>
2022-10-31 12:43:06 +0100

    moving toggle button added

--- src/components/AppContainer/Header/SwitchMode/index.js
+++ src/components/AppContainer/Header/SwitchMode/index.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 import { Wrapper, ToggleText, ToggleButton, MovingCircle } from "./styled";
+import { ReactComponent as Icon } from "./toggle-icon.svg";
 
 export const SwitchMode = () => {
+  const [toggleOn, setToggleOn] = useState(false);
+
+  const SwitchMode = () => 
+    toggleOn === false ? setToggleOn(true) : setToggleOn(false);
+
   return (
     <Wrapper>
       <ToggleText>Dark mode off</ToggleText>
-      <ToggleButton>
-        <MovingCircle></MovingCircle>
+      <ToggleButton onClick={SwitchMode}>
+        <MovingCircle toggleOn={toggleOn}>
+          <Icon />
+        </MovingCircle>
       </ToggleButton>
     </Wrapper>
   );
