8a78212045c463bff62e88452b7625e78b335a99
golosgit <107067096+golosgit@users.noreply.github.com>
2022-11-13 14:48:18 +0100

    styles improved for mobile version

--- src/components/common/Title/index.js
+++ src/components/common/Title/index.js
@@ -12,11 +12,11 @@ export const Title = styled.h2`
     font-size: 18px;
   }
 
-  @media (max-width: 390px) {
+  @media (max-width: 392px) {
     max-width: 240px;
   }
 
-  @media (max-width: 340px) {
+  @media (max-width: 348px) {
     max-width: 190px;
   }
 
